cc02e5fb04bee71ac8d9c45e98dbc143a2e8cc1f
mchardex <mchardex1995@gmail.com>
2019-03-17 10:46:40 +0100

    setup admin post book and user borrow book endpoints

---
+++ server/src/middlewares/validations/postBook.js
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+import validate from '../../helpers/validateInput';
+
+const postBook = (req, res, next) => {
+  const data = req.body;
+
+  const schema = Joi.object().keys({
+    title: Joi.string().min(3).required(),
+    author: Joi.string().min(3).required(),
+    fileurl: Joi.string().max(100).required(),
+    coverimageurl: Joi.string().max(100).required(),
+    description: Joi.string().min(10).max(200).required(),
+  });
+
+  Joi.validate(data, schema, err => validate(err, res, next));
+};
+
+export default postBook;
